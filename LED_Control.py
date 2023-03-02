d337a9af81c31abdea744e2d0b97dd138d1cac11
Stefan-Tasikj <109251915+Stefan-Tasikj@users.noreply.github.com>
2023-03-03 00:30:34 +0100

    Update LED_Control.py

--- LED_Control.py
+++ LED_Control.py
@@ -1,5 +1,6 @@
 from PyP100 import PyL530
 l530 = PyL530.L530("local IP of the Lightbulb", "Email", "password")
+#Connect the P100 device using the tapo app, find the IP address from your router panel
 l530.handshake()
 l530.login()
 alreadyRan=False
